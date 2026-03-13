f01a317ee1945ee62559187184a86010b6ce8331
VoroninaAnastsasia <a.voronina3@g.nsu.ru>
2026-03-13 23:49:07 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,17 +1,15 @@
 def main():
     try:
         with open('input.txt', 'r', encoding='utf-8') as input_file:
-            content = input_file.read()
+            data = input_file.read()
         
-        transformed_content = content.upper()
+        new_data = data.upper()
         
         with open('output.txt', 'w', encoding='utf-8') as output_file:
-            output_file.write(transformed_content)
+            output_file.write(new_data)
             
     except FileNotFoundError:
-        print("Ошибка: Файл input.txt не найден")
-    except Exception as e:
-        print(f"Произошла ошибка: {e}")
+        print("Файл не найден")
 
 if __name__ == "__main__":
     main()
