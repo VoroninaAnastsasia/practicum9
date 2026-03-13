87f12e1a236fd6306c410e75531c816c8973c41b
VoroninaAnastsasia <a.voronina3@g.nsu.ru>
2026-03-14 00:41:35 +0700

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,15 +1,22 @@
 def main():
     try:
         with open('input.txt', 'r', encoding='utf-8') as input_file:
-            lines = input_file.readlines()
-
-        filtered_lines = [line for line in lines if line.strip() != '100']
-
+            all_lines = input_file.readlines()
+        
+        result_lines = []
+        
+        for line in all_lines:
+            clean_line = line.strip()
+            
+            if clean_line != '100':
+                result_lines.append(line)
+        
         with open('output.txt', 'w', encoding='utf-8') as output_file:
-            output_file.writelines(filtered_lines)
+            for result_line in result_lines:
+                output_file.write(result_line)
             
     except FileNotFoundError:
-        print("Файл не найден")
+        print('Файл не найден')
 
 if __name__ == "__main__":
     main()
