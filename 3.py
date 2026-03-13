f88da4496e0c3b24ccb7edd2cbb5806753fec7e5
VoroninaAnastsasia <a.voronina3@g.nsu.ru>
2026-03-14 00:17:25 +0700

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,17 +1,24 @@
 def main():
     try:
         with open('input.txt', 'r', encoding='utf-8') as input_file:
-            # Фильтруем пустые строки и берем первый символ каждой строки
-            first_chars = [line[0] for line in input_file if line.strip()]
+            all_lines = input_file.readlines()
         
-        # Запись результата
+        first_ch_list = []
+        
+        for line in all_lines:
+            clean_line = line.strip()
+            
+            if clean_line != '':
+                first_ch = clean_line[0]
+                first_ch_list.append(first_ch)
+        
+        result_word = ''.join(first_ch_list)
+        
         with open('output.txt', 'w', encoding='utf-8') as output_file:
-            output_file.write(''.join(first_chars))
+            output_file.write(result_word)
             
     except FileNotFoundError:
-        print("Ошибка: Файл input.txt не найден")
-    except Exception as e:
-        print(f"Произошла ошибка: {e}")
+        print('Файл не найден')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
